2d848f519ca0c5b4ca575f10d68319d53520392e
Ramzi Chamcham <ramzichamcham@gmail.com>
2020-03-17 17:00:05 -0400

    Add checkmark accessory to table cells Load data from itemsArray (hard coded)

---
+++ Todoey/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Todoey
+//
+//  Created by Ramzi chamcham on 2020-03-12.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct K{
+    static let reusableIdentifier = "ToDoItemCell"
+}
